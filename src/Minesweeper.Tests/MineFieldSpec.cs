3377f20e12cd17bbb0791a7917a3cd583a21fed8
심승용 <seungyong.shim@mirero.co.kr>
2021-01-03 14:49:30 +0900

    SetBombs 구현

--- src/Minesweeper.Tests/MineFieldSpec.cs
+++ src/Minesweeper.Tests/MineFieldSpec.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoFixture.Xunit2;
 using FluentAssertions;
 using Xunit;
 
@@ -20,17 +22,22 @@ namespace Minesweeper.Tests
             sut.Cells.Length.Should().Be(4 * 4);
         }
 
-        [Fact]
-        public void SetBombs()
+        [Theory]
+        [InlineAutoData]
+        [InlineAutoData]
+        [InlineAutoData]
+        [InlineAutoData]
+        [InlineAutoData]
+        public void SetBombs([Range(3, 5)] int bombCount)
         {
             // Arrange
-            var sut = new MineField(4, 4, 3);
+            var sut = new MineField(4, 4, bombCount);
 
             // Act
             sut.SetBombs();
 
             // Assert
-            sut.Cells.Where(x => x.IsBomb).Count().Should().Be(3);
+            sut.Cells.Where(x => x.IsBomb).Count().Should().Be(bombCount);
         }
     }
 }
--- src/Minesweeper/MineField.cs
+++ src/Minesweeper/MineField.cs
@@ -13,7 +13,9 @@ namespace Minesweeper
             Width = width;
             Height = height;
             BombCount = bombCount;
-            Cells = new Cell[Width * Height];
+            Cells = Enumerable.Range(0, Width * Height)
+                              .Select(x => new Cell())
+                              .ToArray();
         }
 
         public int Width { get; }
@@ -23,7 +25,18 @@ namespace Minesweeper
 
         public void SetBombs()
         {
-            throw new NotImplementedException();
+            var rand = new Random();
+
+            for (int i = 0; i < BombCount; i++)
+            {
+                var curr = Cells[rand.Next(0, Width * Height)];
+
+                if (curr.IsBomb)
+                {
+                    i--;
+                }
+                else curr.SetBomb();
+            }
         }
     }
 }
